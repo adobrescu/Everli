150c5420819b7600e2307e1d3965467900d6cb61
Adi <adi.dobrescu@gmail.com>
2021-09-29 01:51:13 +0300

    Overrides ObservableRepository::createRecord and ::deleteRecord methods to trigger notifications

--- task3/oop/test.php
+++ task3/oop/test.php
@@ -19,6 +19,19 @@ class NotificationSource implements \INotificationSource
 class ObservableRepository extends \Repository implements \INotificationSource, \IRepository
 {
     use \TNotificationSource;
+
+    function createRecord(array $record): int {
+        parent::createRecord($record);
+        $this->callNotificationHandlers('create', $record);
+
+        return (int) key(end($this->records));
+    }
+    
+    function deleteRecord(int $id) {
+        $recordToDelete = $this->records[$id];
+        parent::deleteRecord($id);
+        $this->callNotificationHandlers('delete', $recordToDelete);
+    }
 }
 
 $obj = new NotificationSource();
