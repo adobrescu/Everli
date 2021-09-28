be5ad3290f9297919d33b762bb6d3df20815cf02
Adi <adi.dobrescu@gmail.com>
2021-09-29 02:48:28 +0300

    Removes uneeded code from ObservableRepository::createRecord

--- task3/oop/ObservableRepository.class.php
+++ task3/oop/ObservableRepository.class.php
@@ -7,18 +7,18 @@ class ObservableRepository extends Repository implements INotificationSource, IR
     use TNotificationSource;
 
     function createRecord(array $record): int {
-        parent::createRecord($record);
+        $newRecordKey = parent::createRecord($record);
 
         $this->callNotificationHandlers('create', $record);
 
-        return (int) key(end($this->records));
+        return $newRecordKey;
     }
     
     function deleteRecord(int $id) {
         $recordToDelete = $this->records[$id];
 
         parent::deleteRecord($id);
-        
+
         $this->callNotificationHandlers('delete', $recordToDelete);
     }
 }
