584f6e7498cf83a0b131381440699ea48ed8b3d2
Adi <adi.dobrescu@gmail.com>
2021-09-28 03:30:15 +0300

    Adds rb description

--- task1/reverse_binary.js
+++ task1/reverse_binary.js
@@ -1,6 +1,34 @@
 "use strict";
 
 /**
+ * Reverse bits for a given number.
+ *  
+ * It reverses all or just  number's "significant" bits.
+ * "Significant" bits start with the first bit that has a value of 1.
+ * 
+ *  
+ * Eg. in 64 but representation of 13:
+ * 
+ * 00000  ....... 001101
+ * 
+ * The "significant" bits are the last 4:
+ * 
+ * 1101
+ * 
+ * 
+ * Eg:
+ * 
+ * Given a number on 64 bit: 
+ * 000000 ...... 00001101 (64 bit for "13")
+ * 
+ * - reverse "significant" bits:
+ * 000000 ....... 00001011 (64 bit for "11") 
+ * 
+ * - reverse all bits:
+ * 110100 ....... 00000000
+ * 
+ * Number of bits of a number is 64:
+ * https://www.w3schools.com/js/js_numbers.asp
  * 
  * @param {number} n 
  */
