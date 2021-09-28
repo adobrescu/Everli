cefb49126e9384b1952e803bb608fc62fd0f1b3b
Adi <adi.dobrescu@gmail.com>
2021-09-29 00:49:07 +0300

    Adds INotificationSource & IRepository defining public interface for objects that can emit notifications and a repository that allows only adding a new record, deleting one or reading all records

---
+++ task3/oop/interfaces.php
@@ -0,0 +1,12 @@
+<?php
+
+interface INotificationSource {
+    function registerNotificationHandler($notificationName, $callback);
+}
+
+interface IRepository {
+    function createRecord(array $record): int;
+    function deleteRecord(integer $id);
+
+    function readAll();
+}
