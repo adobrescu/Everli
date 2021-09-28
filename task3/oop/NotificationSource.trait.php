383d7ae18d5f9dbc24be87c55f1c3c2f32e7cb8a
Adi <adi.dobrescu@gmail.com>
2021-09-29 01:53:44 +0300

    Adds second parameter  to NotificationSource::callNotificationHandlers that can be any value to pass to notification handlers

--- task3/oop/NotificationSource.trait.php
+++ task3/oop/NotificationSource.trait.php
@@ -9,13 +9,13 @@ trait TNotificationSource
         $this->notificationHandlers[$notificationName][] = $callback;
     }
 
-    protected function callNotificationHandlers ($notificationName) {
+    protected function callNotificationHandlers ($notificationName, $notification = null) {
         if (!is_array($this->notificationHandlers[$notificationName]) ) {
             return;
         }
 
         foreach ( $this->notificationHandlers[$notificationName] as $callback ) {
-            $callback();
+            $callback($notificationName, $notification);
         }
     }
 }
