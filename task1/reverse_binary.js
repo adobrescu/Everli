1a7f0e1d9b1a5fafd0b5183c89c26b52682b1ac7
Adi <adi.dobrescu@gmail.com>
2021-09-28 03:06:55 +0300

    Adds rb ('reverseBinary') parameter validation as number

--- task1/reverse_binary.js
+++ task1/reverse_binary.js
@@ -5,7 +5,9 @@
  * @param {number} n 
  */
 function reverseBinary(n) {
-
+    if ( typeof(n) != "number" ) {
+        throw new Error("reverseBinary: given parameter must be a number");
+    }
 }
 
 exports.reverseBinary = reverseBinary;
