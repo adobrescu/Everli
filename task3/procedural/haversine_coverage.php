3c3b37fcaa32a9c7d90d820c85e2484e95e9833e
Adi <adi.dobrescu@gmail.com>
2021-09-28 21:40:47 +0300

    Adds task 3 implementation file (contains only 2 function signatures)

---
+++ task3/procedural/haversine_coverage.php
@@ -0,0 +1,26 @@
+<?php
+    $locations = [
+        ['id' => 1000, 'zip_code' => '37069', 'lat' => 45.35, 'lng' => 10.84],
+        ['id' => 1001, 'zip_code' => '37121', 'lat' => 45.44, 'lng' => 10.99],
+        ['id' => 1002, 'zip_code' => '37129', 'lat' => 45.44, 'lng' => 11.00],
+        ['id' => 1003, 'zip_code' => '37133', 'lat' => 45.43, 'lng' => 11.02]
+      ];
+    
+    $shoppers = [
+        ['id' => 'S1', 'lat' => 45.46, 'lng' => 11.03, 'enabled' => true],
+        ['id' => 'S2', 'lat' => 45.46, 'lng' => 10.12, 'enabled' => true],
+        ['id' => 'S3', 'lat' => 45.34, 'lng' => 10.81, 'enabled' => true],
+        ['id' => 'S4', 'lat' => 45.76, 'lng' => 10.57, 'enabled' => true],
+        ['id' => 'S5', 'lat' => 45.34, 'lng' => 10.63, 'enabled' => true],
+        ['id' => 'S6', 'lat' => 45.42, 'lng' => 10.81, 'enabled' => true],
+        ['id' => 'S7', 'lat' => 45.34, 'lng' => 10.94, 'enabled' => true]
+    ];
+    //////////////////////////////
+
+    function haversine($lat1, $long1, $lat2, $long2) {
+
+    }
+
+    function shoppersCoverage($shoppers, $locations) {
+
+    }
