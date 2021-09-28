348a5c5c39d79e34c9ee294ead759bd65400d285
Adi <adi.dobrescu@gmail.com>
2021-09-28 13:04:57 +0300

    Adds a few tests for Path::cd - input not validated

--- task2/change_directory.php
+++ task2/change_directory.php
@@ -7,6 +7,34 @@
     assert($path->currentPath == '/a/b/c/d');
 
     $path->cd('../x');
-    
     assert($path->currentPath == '/a/b/c/x');
+
+    try {
+        $path->cd('../../../../../aa');
+        assert(false, 'Exception not thrown');
+    } catch (Exception $err) {
+        assert($err->getCode() == Path::ERR_INVALID_PATH);
+        assert($path->currentPath == '/a/b/c/x');
+    }
+    
+    $path->cd('../../../../a');
+    assert('/a' == $path->currentPath);
+
+    $path->cd('../');
+    assert('/' == $path->currentPath);
+
+    $path->cd('a');
+    assert('/a' == $path->currentPath);
+
+    $path->cd('.');
+    assert('/a' == $path->currentPath);
+
+    $path->cd('./b/c');
+    assert('/a/b/c' == $path->currentPath);
+
+    $path->cd('./b/c/');
+    assert('/a/b/c/b/c' == $path->currentPath);
+
+    $path->cd('./b/c/../../../../../../x/y');
+    assert('/a/x/y' == $path->currentPath);
     //should display '/a/b/c/x'. 
