88be866ecdc660573bb1b302e934be13512cf05a
Adi <adi.dobrescu@gmail.com>
2021-09-28 13:04:23 +0300

    Implements Path::cd

--- task2/Path.class.php
+++ task2/Path.class.php
@@ -2,12 +2,27 @@
 
 class Path
 {
+    const ERR_INVALID_PATH = 2001;
+
     public $currentPath;
 
     public function __construct($currentPath) {
         $this->currentPath = $currentPath;
     }
     public function cd($relativePath) {
+        $currentPath = $this->currentPath;
+
+        /**
+         * If $relativePath starts  with a "/" then set currentPath to "/" and treat the remaining part as 
+         * a relative path.
+        */
+
+        if ( $relativePath[0] == '/' ) {
+            $currentPath = '/';
+            $relativePath = substr($relativePath, 0);
+        } else {
+            $currentPath = $this->currentPath;
+        }
         /**
          * Use a stack to implement the requirement
          * Initially the stack contains only current path components
@@ -21,6 +36,36 @@ class Path
          *  - if different than ".." then push relative path component in the stack (move to subdirectory)
          *  
          */
+        if ( $currentPath == '/' ) {
+            $stack = [''];
+        } else {
+            $stack = explode('/', $currentPath);
+        }
+        $relativePathComponents = explode('/', $relativePath);
         
+        foreach ( $relativePathComponents as $relativePathComponent) {
+            switch ( $relativePathComponent ) {
+                case '.':
+                case '':
+                    break;
+                case '..':
+                    //move one level up if possible, otherwise throw an exception
+                    if ( count($stack) == 1 ) {
+                        //top level reached
+                        throw new Exception('Root / top level reached', static::ERR_INVALID_PATH);
+                    }
+                    array_pop($stack);
+                    break;
+                default:
+                    array_push($stack, $relativePathComponent);
+            }
+        }
+        
+        if ( count($stack) == 1 ) {
+            // the stack contains the empty element only
+            $this->currentPath = '/';
+        } else {
+            $this->currentPath = implode('/', $stack);
+        }
     }
 }
