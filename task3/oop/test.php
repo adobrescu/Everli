e25b8ded34767373d1ea9fe0c53f6bccd672eb9f
Adi <adi.dobrescu@gmail.com>
2021-09-29 04:00:38 +0300

    Implements coverage calculation in ShoppersCoverageCalculator - needs refactoring

--- task3/oop/test.php
+++ task3/oop/test.php
@@ -17,7 +17,106 @@ class NotificationSource implements \INotificationSource
     }
 }
 
+class ShopperCoverage
+{
+    public $nearByLocationIds;
+}
 
+class ShoppersCoverageCalculator
+{
+    const COVERAGE_MAX_DISTANCE = 10;
+    /*
+        Maximum distance a location is considered covered by a shopper;
+        Different class instances may use different values;
+    */
+    protected $coverageMaxDistance; 
+    protected $shoppers;
+    protected $locations;
+
+    protected $shoppersCoverage = [];
+
+    public function __construct(\IObservableRepository $shoppers, 
+                                \IObservableRepository $locations,
+                                $coverageMaxDistance = ShoppersCoverageCalculator::COVERAGE_MAX_DISTANCE) {
+        $this->shoppers = $shoppers;
+        $this->locations = $locations;
+        $this->coverageMaxDistance = $coverageMaxDistance;
+
+        foreach ($this->shoppers->readAll() as $shopper ) {
+            $this->calculateShopperCoverage($shopper);
+        }
+    }
+
+    public function calculateShopperCoverage($shopper) {
+        if ( !$shopper['enabled'] ) {
+            return;
+        }
+        $shopperId = $shopper['id'];
+
+        if (!isset($this->shoppersCoverage[$shopperId])) {
+            $this->shoppersCoverage[$shopperId] = [];
+        }
+
+        $this->shoppersCoverage[$shopperId] = $this->calculateShopperNearByLocations($shopper, $this->locations->readAll());
+    }
+
+    public function calculateShopperNearByLocations($shopper, $locations) {
+        $nearByLocations = [];
+
+        foreach ( $locations as $location ) {
+            $distance = static::haversine($shopper['lat'], $shopper['lng'], $location['lat'], $location['lng']);
+
+            if( $distance >= $this->coverageMaxDistance ) {
+                continue;
+            }
+
+            $locationId =  $location['id'];
+            $nearByLocations[ $locationId] = $locationId;
+        }
+
+        return $nearByLocations;
+    }
+    public function getAllShoppersCoverage() {
+        $result = [];
+        $numLocations = $this->locations->countAll();
+        
+        foreach ( $this->shoppersCoverage as $shopperId => $nearByLocationIds) {
+            $shopperNumNearByLocations = count($nearByLocationIds);
+
+            $result[] = [ 'shopper_id' => $shopperId, 'coverage' => 100 * $shopperNumNearByLocations / $numLocations];
+        }
+
+        usort($result, [$this, 'static::compareShoppersCoverageDesc']);
+
+        return $result;
+    }
+
+    static public function compareShoppersCoverageDesc($shopper1, $shopper2) {
+        if ( $shopper1['coverage'] > $shopper2['coverage'] ) {
+            return -1;
+        }
+        if ( $shopper1['coverage'] < $shopper2['coverage'] ) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public function haversine($lat1, $lng1, $lat2, $lng2) {
+
+    }
+}
+
+/**
+ * Test class, just overrides the 'haversine' method
+ */
+class TestShoppersCoverageCalculator extends ShoppersCoverageCalculator
+{
+    public function haversine($lat1, $lng1, $lat2, $lng2) {
+        static $numCalls = 0;
+        
+        return ( $numCalls++ % 3 ) * $this->coverageMaxDistance;
+    }
+}
 
 $obj = new NotificationSource();
 $obj->registerNotificationHandler('someEvent', function () {
@@ -40,6 +139,7 @@ assert($numLocations + 1 == count($repo->readAll()));
 
 $repo->deleteRecord($newRecordKey);
 assert($numLocations == count($repo->readAll()));
+
 
 /////////////////////////////////
 
@@ -66,6 +166,29 @@ assert($numRecords0 + 2 == $numRecords);
 $repo->deleteRecord($lastNewKey);
 
 assert($numRecords0 + 1 == $numRecords);
+
+//ShoppersCoverageCalculator tests
+$calculator = new TestShoppersCoverageCalculator(
+    new \ObservableRepository($shoppers),
+    new \ObservableRepository($locations)
+);
+$result = $calculator->getAllShoppersCoverage();
+
+assert(5 == count($result));//7 shoppers - 2 disabled
+
+assert($result[0]['shopper_id'] == 'S1' || $result[0]['shopper_id'] == 'S6' );
+assert($result[1]['shopper_id'] == 'S1' || $result[1]['shopper_id'] == 'S6' );
+
+assert($result[2]['shopper_id'] == 'S3' || $result[0]['shopper_id'] == 'S4'  || $result[0]['shopper_id'] == 'S7');
+assert($result[3]['shopper_id'] == 'S3' || $result[3]['shopper_id'] == 'S4'  || $result[3]['shopper_id'] == 'S7');
+assert($result[4]['shopper_id'] == 'S3' || $result[4]['shopper_id'] == 'S4'  || $result[4]['shopper_id'] == 'S7');
+
+assert(50 == $result[0]['coverage']);
+assert(50 == $result[1]['coverage']);
+
+assert(25 == $result[2]['coverage']);
+assert(25 == $result[3]['coverage']);
+assert(25 == $result[4]['coverage']);
 
 echo 'No failed assertions' . PHP_EOL;
 
